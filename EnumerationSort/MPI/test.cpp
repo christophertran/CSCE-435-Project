7a1ca05a4beccbbdf4f28a734de79a0f28068074
Rohit Barichello <rohitbarichello@gmail.com>
2021-11-19 13:11:15 -0600

    debug

--- EnumerationSort/MPI/test.cpp
+++ EnumerationSort/MPI/test.cpp
@@ -32,19 +32,66 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
     // load random data
-    int *data; // this is the data we're sorting
+    int *data;  // this is the data we're sorting
     unsigned long data_size;
     fill_array_from_binary_file(&data, argv[1], myid, numprocs, data_size);
 
-    int sorted[data_size]; // this is our sorted array
+    int sorted[data_size];  // this is our sorted array
+    int element;            // this is the element assigned to this process
+    int final_index = 0;    // this is the index of the element assigned to this process
+                            // in the final sorted array
 
-    if(myid == 0) {
-        printf("Sorted Data: \n");
-        for(int i = 0; i < data_size; i++) {
-            printf("%d: %d \n", i, data[i]);
+    // start sorting
+    if (myid == 0) {  // main proc
+        printf("MAIN PROCESS\n");
+        int number;
+
+        for (int i = 0; i < data_size; ++i) {  // i represents id of process
+            number = data[i];
+
+            // printf(" %d \n", number);
+
+            // send value to corresponding proc's reg X
+            if (i != 0) {  //if not the main process, send the number to process i
+                MPI_Send(&number, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            } else {
+                element = number;  // if process is main, element is first one in input array
+            }
+
+            // decide if incrementing this processes/elements final index in the sorted array
+            final_index += (element > number);
+        }
+        printf("\n");
+    } else {  // sub procs
+        // printf("SUB PROCESS\n");
+        MPI_Recv(&element, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &stat);
+
+        // do comparsion
+        for (int i = 0; i < data_size; ++i) {
+            // MPI_Recv(&reg_y, 1, MPI_INT, myid - 1, REG_TAG_Y, MPI_COMM_WORLD, &stat);
+            number = data[i];
+
+            if (i != myid) {
+                final_index += (element > number);
+            }
         }
     }
 
-    // MPI_Finalize();
+    // wait for all processses to calculate their element's final index
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // assign element to sorted array
+    sorted[final_index] = element;
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (myid == 0) {
+        printf("Sorted Data: \n");
+        for (int i = 0; i < data_size; i++) {
+            printf("%d: %d \n", i, sorted[i]);
+        }
+    }
+
+    MPI_Finalize();
     return 0;
 }
